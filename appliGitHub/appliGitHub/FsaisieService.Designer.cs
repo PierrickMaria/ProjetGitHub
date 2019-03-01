f6762733c179d86e81d3b0735c6f91369da84a2a
aetech97 <48093198+aetech97@users.noreply.github.com>
2019-03-01 09:23:26 +0100

    SaisieService

---
+++ appliGitHub/appliGitHub/FsaisieService.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace appliGitHub
+{
+    partial class FsaisieService
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id_lbl = new System.Windows.Forms.Label();
+            this.btn_valider = new System.Windows.Forms.Button();
+            this.btn_annuler = new System.Windows.Forms.Button();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // id_lbl
+            // 
+            this.id_lbl.AutoSize = true;
+            this.id_lbl.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id_lbl.Location = new System.Drawing.Point(51, 34);
+            this.id_lbl.Name = "id_lbl";
+            this.id_lbl.Size = new System.Drawing.Size(21, 19);
+            this.id_lbl.TabIndex = 0;
+            this.id_lbl.Text = "id";
+            // 
+            // btn_valider
+            // 
+            this.btn_valider.Location = new System.Drawing.Point(55, 173);
+            this.btn_valider.Name = "btn_valider";
+            this.btn_valider.Size = new System.Drawing.Size(75, 23);
+            this.btn_valider.TabIndex = 1;
+            this.btn_valider.Text = "Valider";
+            this.btn_valider.UseVisualStyleBackColor = true;
+            // 
+            // btn_annuler
+            // 
+            this.btn_annuler.Location = new System.Drawing.Point(163, 173);
+            this.btn_annuler.Name = "btn_annuler";
+            this.btn_annuler.Size = new System.Drawing.Size(75, 23);
+            this.btn_annuler.TabIndex = 2;
+            this.btn_annuler.Text = "Annuler";
+            this.btn_annuler.UseVisualStyleBackColor = true;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_id.Location = new System.Drawing.Point(78, 31);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(160, 27);
+            this.txt_id.TabIndex = 3;
+            // 
+            // FsaisieService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 220);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.btn_annuler);
+            this.Controls.Add(this.btn_valider);
+            this.Controls.Add(this.id_lbl);
+            this.Name = "FsaisieService";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Saisie service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label id_lbl;
+        private System.Windows.Forms.Button btn_valider;
+        private System.Windows.Forms.Button btn_annuler;
+        private System.Windows.Forms.TextBox txt_id;
+    }
+}
+
--- appliGitHub/appliGitHub/FsaisieService.cs
+++ appliGitHub/appliGitHub/FsaisieService.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace appliGitHub
 {
-    public partial class Form1 : Form
+    public partial class FsaisieService : Form
     {
-        public Form1()
+        public FsaisieService()
         {
             InitializeComponent();
         }
